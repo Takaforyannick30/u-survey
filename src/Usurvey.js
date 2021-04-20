d3b66de6fb9b70fedc95ec4ed665b98b927f885e
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-21 01:49:48 +0200

    Created Usurvey component

---
+++ src/Usurvey.js
@@ -0,0 +1,13 @@
+import React, { Component } from'react';
+
+class Usurvey extends Component {
+    render() {
+        return(
+          <div>
+            <h1>Usurvery App</h1>
+          </div>
+        );
+    }
+}
+
+export default Usurvey;
